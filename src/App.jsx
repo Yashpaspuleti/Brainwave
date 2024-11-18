1ad07c0ba3c9fb1eae2b1e1c46e4b11457245441
yashwanth kumar <yash.paspuleti@gmail.com>
2024-11-18 18:12:31 +0530

    Pricing and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-import Pricing from "./Pricing";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,8 @@
-import { smallSphere, stars } from "./assets";
-import Section from "./components/Section";
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import Section from "./Section";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
@@ -22,6 +25,25 @@ const Pricing = () => {
               alt="Stars"
             />
           </div>
+        </div>
+
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+        />
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
         </div>
       </div>
     </Section>
